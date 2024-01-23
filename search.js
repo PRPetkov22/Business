75d3e3f590b1ab049a0f5140b49498768bbeacbb
Plamen Raykov Petkov <132440546+PRPetkov22@users.noreply.github.com>
2024-01-23 22:11:03 +0200

    Update search.js

--- search.js
+++ search.js
@@ -34,16 +34,21 @@ document.getElementById('search-input').addEventListener('input', function() {
         "verheiratet - семеен","alt - стар","ledig - несемеен","verwitwen - вдовец","das Alter - възраст","die Hauptstadt-е - столица",
         "also - следователно","hier - тук","dort - там","aber - но","die Polizei - полиция","schon - красив","lange - дълго време","die Arzt-e - лекар",
         "der partner - партньор","zurzeit - за момента","die Banane-n - банан","die Butter - масло","das Ei-er - яйце","das Mehl - брашно",
-        "die Milch - мляко","der Zucker - захар","der Pfannkuchen - палачинка","die Schokolade - шоколад",
-        "Ich komme; du kommst; er/sie/es kommt; wir kommen; ihr kommt; Sie/sie kommen",
-        "Ich spreche; du sprichst; er/sie/es spricht; wir sprechen; ihr sprecht; Sie/sie sprechen",
-        "Ich heiBe; du heiBt; er/sie/es heiBt; wir heiBen; ihr heiBt; Sie/sie heiBen",
-        "Ich bin; du bist; er/sie/es ist; wir sind; ihr seid; Sie/sie sind",
-        "Ich habe; du hast; er/sie/es hat; wir haben; ihr habt; Sie/sie haben",
-        "Ich lebe; du lebst; er/sie/es lebt; wir leben; ihr lebt; Sie/sie leben",
-        "Ich wohne; du wohnst; er/sie/es wohnt; wir wohnen; ihr wohnt; Sie/sie wohnen",
-        "Ich lerne; du lernst; er/sie/es lernt; wir lernen; ihr lernt; Sie/sie lernen",
-        "mein - моя","dein - твоя","ihr - неговия","---"
+        "die Milch - мляко","der Zucker - захар","der Pfannkuchen - палачинка","die Schokolade - шоколад","kaufen - купувам","der Hunger - гладен съм","der Euro - Евро",
+        "dann - тогава","die Welt-en - свят","der Apfel - ябълка","die Orange-n - портокал","der Kuchen - сладкиш","der Kaffee - кафе","der Saft-e - сок",
+        "das Brotchen - хлебче","das Wurstchen - наденичка","die Birne-n - круша","die Tomate-n - домат","das Brot - хляб","die Kartoffel-n - картов",
+        "der Joghurt-s - кисело мляко","die Zwiebel-n - лук","lecker - вкусен","etwas - нещо","noch einmal - още веднъж","sonst - в против случай","ncoh - още",
+        "der Fisch-e - риба","das Regal-e - рафт","der Kase - сирене","das Salz - сол","der Tee-s - чай","das Obst - плодове","das Gemuse - зеленчуци","das Mineralwasser - мин вода",
+        "das Wasser - вода","das Fleisch - месо","der Reis - ориз","der Wein-e - вино","das Bier-е - бира","der Preis-e - цена","der Cent - цент","der Prospekt-e - брошура",
+        "das Sonderangebot-e - промоция","das Lebensmittel - хранителни продукти","die Abteilung-en - отдел","die Wurst-e - наденица","das Hackfleisch - кайма",
+        "das Ol-e - олио","wie viel - колко","kosten - струва","das Kilo(gramm) - килограм","das Gramm - грам","das Pfund - половин кило","der Liter - литър",
+        "Becher - чаша","die Packung - опаковка","die Flasche-n - бутилка","die Dose-n - кенче","die Sahne - сметана","das Restaurant-s - ресторант","die Mensa Mensen - столова",
+        "essen - ям","das Steak-s - пържола","der Salat-s - салата","die SoBe-n - сос","das Hahnchen - пиле","die Pommes frites - пържени картофи","die Pizza Pizzen - пица",
+        "das Cola-s - кола","der Durst - жаден съм","kochen - готвя","fur - за","Lieblings - любим","das Essen - ядене","das Rezept-e - рецепта","typisch - типичен",
+        "ganz - цял","einfach - обикновен, прост","die Portion-en - порция","groB - голям(за хора)","schmecken - нещо е вкусно","trinken - пия","das Glas-er - стъкло,чаша",
+        "studieren - следвам(студенти)","die Suppe-n - супа","die Leute - хора","zu hause - вкъщи","nach hause - вкъщи","zum Beispiel - например","der Pfeffer - черен пипер",
+        "reichen - абсурд","der Tag-е - ден","teuer - скъп","billig - евтин","gern-e - с удоволствие","das Getrank-e - напитка","---"
+        
     ];
 
     words.forEach(function(word) {
